6fda715d32f1ffda2b07ab5be0d3ec8a3fc96c74
bsixstyle <ihsan.bsix@gmail.com>
2019-12-27 01:01:38 +0700

    Updates

---
+++ precache-manifest.7fdff7b1fcc38a9b97dd7c5804e5ecda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59b651b0b001a9830fcfb2c30523e579",
+    "url": "/neon/index.html"
+  },
+  {
+    "revision": "5108475f54ba1394cbeb",
+    "url": "/neon/static/css/main.7c9baceb.chunk.css"
+  },
+  {
+    "revision": "7a3927755f734e54b949",
+    "url": "/neon/static/js/2.34bcea92.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/neon/static/js/2.34bcea92.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5108475f54ba1394cbeb",
+    "url": "/neon/static/js/main.5571820e.chunk.js"
+  },
+  {
+    "revision": "6b156debbf3b114fa089",
+    "url": "/neon/static/js/runtime-main.416287b6.js"
+  }
+]);
